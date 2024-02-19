6da634dd0c7b04288c5ed2cef774175e8461d4a1
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-02-19 16:22:38 +0800

    imagesss

---
+++ sms-review/src/components/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Image = () => {
+  return (
+    <img src='./src/assets/logo-academe-sms.png' alt='academe-sms-logo' className='w-[100px] h-[30px]'/>
+  )
+}
+
+export default Image
